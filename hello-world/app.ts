e8946d5d4bfbe08c26f809fee198e74d6634736a
iadokonat <iadokonat0106@gmail.com>
2023-11-21 09:41:52 +0900

    settings

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -10,12 +10,15 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
  *
  */
 
+const timestamp = Date.now();
+
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
         return {
             statusCode: 200,
             body: JSON.stringify({
                 message: 'hello world',
+                timestamp,
             }),
         };
     } catch (err) {
